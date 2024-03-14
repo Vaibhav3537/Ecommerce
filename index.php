1f12e46979c04c8e31b187065eec92c55ac4b03d
Vaibhav3537 <163386093+Vaibhav3537@users.noreply.github.com>
2024-03-14 12:17:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+<html>
+<body>
 
+<form action="index2.php" method="POST">
+enter a nmber: <input type="text" name="number1"><br>
+enter a nmber: <input type="text" name="number2"><br>
+<input type="submit">
+</form>
+
+<?php
+$num1=$_POST['number1'];
+$num2=$_POST['number2'];
+
+if($num1>$num2){
+	echo "num1 is greater";
+}
+else{
+	echo "num2 is greater";
+}
+?>
+</body>
+</html>
